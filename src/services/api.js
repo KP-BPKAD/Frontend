45ba79adfba65a7c29c61c58cd0570bda322d629
Thorix Rover <thorixrover@gmail.com>
2026-03-27 15:55:22 +0900

    pesan commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // 🔴 HARDCODE URL BACKEND — JANGAN DIUBAH LAGI
-const API_BASE_URL = 'https://backend-production-b08e.up.railway.app/api';
+const API_BASE_URL = 'https://backend-production-b08e.up.railway.app/api/v1';
+
 
 const api = axios.create({
   baseURL: API_BASE_URL,
